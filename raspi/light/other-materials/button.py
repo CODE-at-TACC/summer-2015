6b186a3e729bd187c121e0a9f558eb778518b1a9
Matthew Vaughn <vaughn@tacc.utexas.edu>
2015-06-03 20:34:58 -0500

    Added info to configure on connecting to Wifi

--- raspi/light/other-materials/button.py
+++ raspi/light/other-materials/button.py
@@ -4,8 +4,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN)
 
 while True:
     input_state = GPIO.input(18)
