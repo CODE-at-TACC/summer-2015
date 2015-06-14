087b76f80a26e9098fb290c1f1cb550d93ff16bb
jychuah <jychuah@gmail.com>
2015-06-13 19:49:25 -0500

    Added base programs for robotics lessons. Lessons are integrated into Google Slides presentation at: https://docs.google.com/presentation/d/1sJBh-vAq9Lrjjdmzs-TufLAsjmxu6_1yn4OmSHNHlGI/edit?usp=sharing

---
+++ robotics/python/dance.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import time
+import random
+from BrickPi import *                                   
+from TACC_Pi import *
+
+BrickPiSetup()  # setup the serial port for communication
+
+BrickPi.MotorEnable[PORT_B] = 1 #Enable the left motor
+BrickPi.MotorEnable[PORT_C] = 1 #Enable the right motor
+#BrickPi.SensorType[PORT_1] = TYPE_SENSOR_ULTRASONIC_CONT
+#BrickPi.SensorType[PORT_4] = TYPE_SENSOR_TOUCH
+BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
+BrickPi.Timeout = 100   #Set timeout value for the time till which to run the motors after the last command is sent
+BrickPiSetTimeout()     #Set the timeout
+
+
+BrickPi.MotorSpeed[PORT_B] = 255
+BrickPi.MotorSpeed[PORT_C] = 255
+
+BrickPiUpdateValues()
+
+time.sleep(5)
+
+BrickPi.MotorSpeed[PORT_B] = 0
+BrickPi.MotorSpeed[PORT_C] = 0
+
+BrickPiUpdateValues()
---
+++ robotics/python/walkingstick.py
@@ -0,0 +1,23 @@
+import time
+import random
+from BrickPi import *
+from TACC_Pi import *
+
+BrickPiSetup()  # setup the serial port for communication
+
+
+BrickPi.MotorEnable[PORT_B] = 1 #Enable the left motor
+BrickPi.MotorEnable[PORT_C] = 1 #Enable the right motor
+BrickPi.SensorType[PORT_1] = TYPE_SENSOR_ULTRASONIC_CONT
+BrickPi.SensorType[PORT_4] = TYPE_SENSOR_TOUCH
+BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
+
+
+while True:
+    distance = Read_NXT_Ultrasonic(PORT_1)
+    if distance > 100:
+        BrickPi.Motor[PORT_B] = 255
+    else:
+        BrickPi.Motor[PORT_B] = 0
+    BrickPiUpdateValues()
+    time.sleep(0.1)
