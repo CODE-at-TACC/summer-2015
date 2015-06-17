38f650f5fd62b047e80e2ec16edf276b42f0704c
jychuah <jychuah@gmail.com>
2015-06-17 10:12:48 -0500

    Modified setup2.sh to copy TACC_Pi.py to home directory to allow remote execution

--- robotics/setup2.sh
+++ robotics/setup2.sh
@@ -4,4 +4,5 @@ cd /home/pi
 git clone https://github.com/DexterInd/BrickPi_Python.git
 cd BrickPi_Python
 sudo python setup.py install
+cp ./python/TACC_Pi.py ~
 echo "You should be all ready to go!"
