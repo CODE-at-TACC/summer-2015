13e28e47c18e1ab21d401abfad183ff6f2d3414a
Greg Zynda <zyndagj@users.noreply.github.com>
2015-07-08 14:41:22 -0500

    Added netatalk - Fixes #3

--- parallel/install_processing.sh
+++ parallel/install_processing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install oracle-java7-jdk -y
+apt-get -y install oracle-java7-jdk
+apt-get -y install netatalk
 cd /opt
 pTar=processing-2.2.1-linux32.tgz
 wget http://download.processing.org/$pTar
