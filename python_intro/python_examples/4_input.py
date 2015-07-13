0f6d27b643fbfedf4824befd2baca52e4b57d243
jychuah <jychuah@gmail.com>
2015-07-13 14:21:34 -0500

    simplified input

--- python_intro/python_examples/4_input.py
+++ python_intro/python_examples/4_input.py
@@ -1,10 +1,6 @@
-def get_name():
-    typed_name = raw_input( "What is your name? ")
-    return typed_name
-
 def greet( name ):
     print "Hello " + name + "!"
 
-your_name = get_name()
+your_name = raw_input("What is your name? ")
 
 greet( your_name )
